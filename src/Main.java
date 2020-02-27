17f4f9122d6ebef25d7c79d14031a1c436f5ac44
ruben <r.j.scheedler@student.rug.nl>
2020-02-27 14:37:54 +0100

    Added JGit code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Test");
+        Git git;
+        try {
+            git = Git.cloneRepository()
+                    .setURI("https://github.com/HouariZegai/Calculator.git")
+                    .setDirectory(new File("projects/calculator"))
+                    .call();
+
+            Repository repository = git.getRepository();
+            var x = repository.getWorkTree();
+        } catch (Exception e) {
+            System.out.println("Something went wrong cloning the repo");
+        }
+
+
     }
 }
